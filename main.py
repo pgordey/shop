8d5b240729c5bef6fc28cd368d3de268e64c8759
pgordey <gordey1233@gmail.com>
2023-07-23 22:27:47 +0300

    change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import app as app
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 from flask import request, current_app
@@ -8,9 +8,11 @@ from flask import request, current_app
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy()
 
-class item(db.Model):
+class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Integer, nullable=False)
@@ -20,7 +22,34 @@ class item(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+        return render_template('index.html',)
+
+
+@app.route('/general')
+def general():
+        return render_template('general.html',)
+
+
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Получилась ошибка"
+
+
+    else:
+        return render_template('create.html')
 
 @app.route('/about')
 def about():

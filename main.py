b6788163038897740813452aaaa2dd43c4bf0cb2
pgordey <gordey1233@gmail.com>
2023-07-23 14:48:33 +0300

    change

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from flask import Flask
+import app as app
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+from flask import request, current_app
+
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy()
+
+class item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    active = db.Column(db.Boolean, default=True)
+    text = db.Column(db.Text, nullable=False)
 
 
 @app.route('/')
-
-
 def index():
-    return "Привет"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
